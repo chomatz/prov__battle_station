a031b9a801dcf0526750e9537581c87c5dcdcfe4
chomatz <chomatz@gmail.com>
2024-02-18 23:12:53 +0800

    added installation and configuration of additional vim plugins

--- templates/powerline-vim.sh
+++ templates/powerline-vim.sh
@@ -6,5 +6,26 @@ if [ ! -f "$out_file" ]; then
 	echo "set noshowmode" >> "$out_file"
 	echo "set t_Co=256" >> "$out_file"
 	echo "set mouse-=a" >> "$out_file"
+	echo "autocmd FileType yaml setlocal foldmethod=indent" >> "$out_file"
+	echo "set foldlevelstart=9999" >> "$out_file"
+	echo "nnoremap <space> za" >> "$out_file"
+	if [ ! -d "~/.vim/pack/Yggdroot/start/indentLine" ]; then
+		git clone https://github.com/Yggdroot/indentLine.git ~/.vim/pack/Yggdroot/start/indentLine
+	elif [ -d "~/.vim/pack/Yggdroot/start/indentLine" ]; then
+		cd ~/.vim/pack/Yggdroot/start/indentLine
+		git pull
+	fi
+	vim -u NONE -c "helptags  ~/.vim/pack/Yggdroot/start/indentLine/doc" -c "q"
+	echo "autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab" >> "$out_file"
+	echo "let g:indentLine_char = '|'" >> "$out_file"
+	echo "nnoremap <tab> :IndentLinesToggle <bar> :set number!<CR>" >> "$out_file"
+	echo "set number" >> "$out_file"
+	if [ ! -d "~/.vim/pack/tpope/start/fugitive" ]; then
+		git clone https://tpope.io/vim/fugitive.git ~/.vim/pack/tpope/start/fugitive
+	elif [ -d "~/.vim/pack/tpope/start/fugitive" ]; then
+		cd ~/.vim/pack/tpope/start/fugitive
+		git pull
+	fi
+	vim -u NONE -c "helptags  ~/.vim/pack/tpope/start/fugitive/doc" -c "q"
 fi
 alias vi='vim'
